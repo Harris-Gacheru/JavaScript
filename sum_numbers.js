20734acb401b511dd37b57b4a5f590e86b58957f
Harris <harriskarume.hk@gmail.com>
2022-04-08 12:55:37 +0300

    Return sum of set of numbers

--- sum_numbers.js
+++ sum_numbers.js
@@ -1,14 +1,12 @@
 // Write a program that takes a set of numbers and return the sum of all the numbers passed
 
-const sum_numbers = (numbers) => {
+const printSum = (...nums) => {
     let sum = 0;
-
-    // loop through array of numbers 
-    for (let i = 0; i < numbers.length; i++) {
-        sum = sum + numbers[i];      
-    }
     
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];        
+    }
     return sum;
 }
 
-console.log(sum_numbers([5,4,2]));
+console.log(printSum(5,4,2));
