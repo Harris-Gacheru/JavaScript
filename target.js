9b1966f0ad26455c8bdbf3abf4ecc6479413ed1d
Harris <harriskarume.hk@gmail.com>
2022-04-10 11:25:39 +0300

    return indices of numbers that add upto the target

--- target.js
+++ target.js
@@ -1,16 +1,32 @@
-// Given an array of integers nums and an integer target, return indices of the two numbers that up to target
+// Given an array of integers nums and an integer target, return indices of the two numbers that add up to target
 
 const sum_target = (nums, target) => {
-    // loop through nums to select the first operand
-    for(let i = 0; i < nums.length; i++){
-        // get second operand
-        for(let j = 0; j < nums.length; j++){
-            // check if sum of the two operands equals the target
-            if ((nums[i] + nums[j]) === target) {
-                console.log(`[${i}, ${j}]`);
-            }
-        }
-    }
-}
+    const b4Sort = [...nums];   // before sort
+    nums.sort((a,b) => a -b);   // sort array
+    let index1, index2;
+    let indices = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > target) return;
 
-sum_target([1,2,1,5,7], 7);
+        for (let j = 0; j < nums.length; j++) {
+            if (i != j) {   // avoid repeating the same index       
+                if ((nums[i] + nums[j]) === target) {
+                    if (nums[i] === nums[j]) {  // if operands have the same value
+                        index1 = b4Sort.indexOf(nums[i]);
+                        index2 = b4Sort.lastIndexOf(nums[j]);
+                        indices.push([index1, index2]);
+                    } else {
+                        index1 = b4Sort.indexOf(nums[i]);
+                        index2 = b4Sort.indexOf(nums[j]);
+                        indices.push([index1, index2]);
+                    }
+                }  
+            }
+        }      
+        // console.log(indices); 
+    }
+    return indices;
+};
+
+// sum_target([3,1,2,2,4,5,8,11], 4);
